84eb38d804f4a5d1b6d853296931ba394444a362
hmmftg <h.malekmohammadi@pooya.ir>
2025-08-04 18:31:25 +0330

    chore: remove ai generated sources

--- src/providers/data-provider.ts
+++
@@ -1,4 +0,0 @@
-import { dataProvider as supabaseDataProvider } from "@refinedev/supabase"
-import { supabaseClient } from "@/utility/supabaseClient"
-
-export const dataProvider = supabaseDataProvider(supabaseClient)
